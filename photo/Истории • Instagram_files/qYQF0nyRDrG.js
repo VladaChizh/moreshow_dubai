4d9c5794723ecb8807e11f26942c16199816c894
Влада Чижевская <vladachizh@MacBookPro.lan>
2026-03-19 07:03:46 +0000

    Add MORE SHOW Dubai presentation (RU + EN)

---
+++ photo/Истории • Instagram_files/qYQF0nyRDrG.js
@@ -0,0 +1,3 @@
+;/*FB_PKG_DELIM*/
+
+__d("MAWSupportedVideoTypes",["cometIsMimeTypeForMedia"],(function(t,n,r,o,a,i,l){"use strict";var e=["video/x-ms-wmv","video/x-msvideo","video/avi","video/x-flv","video/ogg","video/mpeg"];function s(t){return o("cometIsMimeTypeForMedia").isMimeTypeForVideo(t)?!e.includes(t):!1}l.isSupportedVideoFileType=s}),98);
